6755cf606ce761957ab6ba3037fa146a96e6b7ce
miltonmb <milton.pasos@gmail.com>
2016-08-12 14:58:39 -0600

    El metodo de ruffini funciona

--- main.cpp
+++ main.cpp
@@ -7,29 +7,52 @@ int main(){
 	int op = 0;
 	int size = 0;
 	int numero = 0;
+	void ruffini(int);
 	while(r== 's'|| r=='S'){
 		cout << "1.Ejercicio 1\n"<<"2.Ejercicio 2\n"<<"3.Salir \n";
 		cout << "Ingrese su opcion: \n";
 		cin>> op;
 		switch(op){
 			case 1:{
-				cout<<"Ingrese el grado mas alto del polinomio: ";
+				cout << "Ingrese el nivel mas alto del coeficiente:\n";
 				cin >> size;
-				cout<<endl;
-				int arreglo[size];
-				for(int i = size; i>=0;i--){
-					cout <<"Ingrese el polinomio de x^"<<i<<":\n";
-					cin >> arreglo[i];
-				}
-			
+				ruffini(size);
+	
+			}			
 				break;
-			}
+			
+
 			case 2:{
 				break;
 			}
 
 			default:{
+				break;
 			}
 		}
 	}
+}
+
+void ruffini(int x){
+	int arreglo[x];
+	int arreglo_dos[x];
+	int a = 0;
+	for(int i = x; i>=0;i--){
+		cout <<"Ingrese el polinomio de x^"<<i<<":\n";
+		cin >> arreglo[i];
+	}
+	cout << "Ingrese a: ";
+	cin >> a;
+	cout <<endl;
+	for(int i = x; i>=0; i--){
+		if(i == x){
+			arreglo_dos[i] = arreglo[i];
+		i--;	
+		}
+		arreglo_dos[i] = a*arreglo_dos[i+1]+arreglo[i];
+	}
+	for(int i = x; i>=0;i--){
+		cout << arreglo_dos[i]<<endl;
+	}
+
 }
